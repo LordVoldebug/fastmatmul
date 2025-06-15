f1812f0c4ffbfaeb98e9eeaec59194f488b9d5ca
Vladimir Kaurkin <kaurkin-vova@yandex.ru>
2025-06-15 18:48:34 +0000

    Implement sparse linear transformation

--- library/matrix/sparse_linear_transformation.h
+++ library/matrix/sparse_linear_transformation.h
@@ -7,23 +7,28 @@
 
 
 namespace linalg_lib::detail {
-
-template<typename MatrixElement>
+template <typename MatrixElement>
 struct LinearTransformationEntry {
   Index row;
   Index col;
   MatrixElement value;
 };
 
-template<typename MatrixElement>
+template <typename MatrixElement>
 class SparseLinearTransformation {
-  using LinearTransformationEntryType = LinearTransformationEntry<MatrixElement>;
+  using LinearTransformationEntryType = LinearTransformationEntry<MatrixElement>
+  ;
 
-  SparseLinearTransformation(Size rows, Size cols) : rows_(rows), cols_(cols) {}
+public:
+  SparseLinearTransformation(Size rows, Size cols) : rows_(rows), cols_(cols) {
+  }
 
   void AddEntry(Index row, Index col, MatrixElement value) {
+    assert(0 <= row && row < Rows());
+    assert(0 <= col && col < Cols());
     elements_.push_back({row, col, value});
   }
+
   // Надо ли в какой то момент проверять что все различные?
   // В какой? Это долго просто, что-то хэшировать или сортировать
   // так для пар еще и встроенного хэшера для хэшмапы нет
@@ -41,24 +46,35 @@ class SparseLinearTransformation {
   }
 
   template <MatrixOrViewType Matrix>
-  friend Matrix operator*(const Matrix& lhs, const SparseLinearTransformation& rhs) {
+  friend UnderlyingMatrixType<Matrix> operator*(const Matrix& lhs,
+                                                const SparseLinearTransformation
+                                                & rhs) {
     assert(lhs.Cols() == rhs.Rows());
-    Matrix res(lhs.Rows(), rhs.Cols());
+    UnderlyingMatrixType<Matrix> res(lhs.Rows(), rhs.Cols());
+    for (auto [rhs_row, rhs_col, rhs_val] : rhs.elements_) {
+      for (Index lhs_row = 0; lhs_row < lhs.Rows(); ++lhs_row) {
+        res(lhs_row, rhs_col) += lhs(lhs_row, rhs_row) * rhs_val;
+      }
+    }
+    return res;
   }
 
   template <MatrixOrViewType Matrix>
-  friend Matrix operator*(const SparseLinearTransformation& lhs, const Matrix& rhs) {
+  friend UnderlyingMatrixType<Matrix> operator*(
+      const SparseLinearTransformation& lhs, const Matrix& rhs) {
     assert(lhs.Cols() == rhs.Rows());
-    Matrix res(lhs.Rows(), rhs.Cols());
-
+    UnderlyingMatrixType<Matrix> res(lhs.Rows(), rhs.Cols());
+    for (auto [lhs_row, lhs_col, lhs_val] : lhs.elements_) {
+      for (Index rhs_col = 0; rhs_col < rhs.Cols(); ++rhs_col) {
+        res(lhs_row, rhs_col) += lhs_val * rhs(lhs_col, rhs_col);
+      }
+    }
+    return res;
   }
-
 
 private:
   std::vector<LinearTransformationEntryType> elements_;
   Size rows_;
   Size cols_;
-
 };
-
 } // namespace linalg_lib::detail
