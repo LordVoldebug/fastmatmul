71579901383e9ea2ec04df3c4df1e6f3eabc4aef
Vladimir Kaurkin <kaurkin-vova@yandex.ru>
2025-06-18 07:57:45 +0000

    Better assymptotics Givens for sparse matrices

--- library/algo/givens_qr.h
+++ library/algo/givens_qr.h
@@ -2,6 +2,7 @@
 #include "matrix/matrix.h"
 #include "utils/types.h"
 #include "givens.h"
+#include "utils/arithmetics.h"
 
 namespace linalg_lib {
 template <OwnedMatrixType Matrix>
@@ -10,6 +11,9 @@ QRResult<Matrix> GivensQR(const Matrix& matrix) {
   Matrix q_suffix = Matrix::Unit(matrix.Rows());
   for (Index col = 0; col < r_converge.Cols(); ++col) {
     for (Index row = col + 1; row < r_converge.Rows(); ++row) {
+      if (IsEpsilonEqualZero(r_converge(row, col))) {
+        continue;
+      }
       auto transformation = detail::GivensRotation(
           col, row, r_converge(col, col), r_converge(row, col));
       transformation.ApplyLeft(r_converge);
