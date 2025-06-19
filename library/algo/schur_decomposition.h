5ce58fa83b5d846429aadfcbf033b8acaa151ecb
Vladimir Kaurkin <kaurkin-vova@yandex.ru>
2025-06-19 14:27:50 +0000

    SchurWilkinsonQR

--- library/algo/schur_decomposition.h
+++ library/algo/schur_decomposition.h
@@ -5,13 +5,14 @@
 #include "utils/arithmetics.h"
 
 namespace linalg_lib {
-
 template <MatrixType Matrix>
-QRResult<Matrix> SchurSimpleQR(const Matrix& matrix, IterCount max_iterations = kDefaultIterations) {
+SchurResult<Matrix> SchurSimpleQR(
+    const Matrix& matrix, IterCount max_iterations = kDefaultIterations) {
   assert(IsSquare(matrix) && "Schur Decomposition is for square matrices");
   auto [q_prefix, r_converge] = HessenbergDecomposition(matrix);
-  for (IterCount iter = 0; iter < max_iterations && !IsUpperTriangular(matrix); ++iter) {
-    auto [q_qr, r_qr] = GivensQR(r_converge); // O(n^2)
+  for (IterCount iter = 0; iter < max_iterations && !IsUpperTriangular(matrix);
+       ++iter) {
+    auto [q_qr, r_qr] = GivensQR(r_converge);  // O(n^2)
     // т. к. O(n) элементов под главной диагональю, и новые не появляются
     r_converge = r_qr * q_qr;
     q_prefix *= q_qr;
@@ -20,19 +21,60 @@ QRResult<Matrix> SchurSimpleQR(const Matrix& matrix, IterCount max_iterations = 
 }
 
 template <MatrixType Matrix>
-QRResult<Matrix> SchurRayleighQR(const Matrix& matrix, IterCount max_iterations = kDefaultIterations) {
+SchurResult<Matrix> SchurRayleighQR(
+    const Matrix& matrix, IterCount max_iterations = kDefaultIterations) {
   assert(IsSquare(matrix) && "Schur Decomposition is for square matrices");
   auto [q_prefix, r_converge] = HessenbergDecomposition(matrix);
-  for (Size prefix = matrix.Rows(); prefix > 1; --prefix) {
-    while (!IsEpsilonEqualZero(r_converge(prefix - 1, prefix - 2))) {
-      auto q_prefix_view = q_prefix.MutView().SubMatrix(Index{0}, Index{0}, prefix, prefix);
-      auto r_converge_view = r_converge.MutView().SubMatrix(Index{0}, Index{0}, prefix, prefix);
-
-      auto shift = r_converge_view(prefix - 1, prefix - 1);;
-      auto [q_qr, r_qr] = GivensQR(r_converge_view - shift * Matrix::Unit(prefix)); // O(n^2)
+  auto rows = matrix.Rows();
+  for (Size minor = rows; minor > 1; --minor) {
+    for (IterCount iter = 0;
+         iter < max_iterations &&
+         !IsEpsilonEqualZero(r_converge(minor - 1, minor - 2));
+         ++iter) {
+      auto shift = r_converge(minor - 1, minor - 1);
+      auto [q_qr, r_qr] =
+          GivensQR(r_converge - shift * Matrix::Unit(rows));  // O(n^2)
       // т. к. O(n) элементов под главной диагональю, и новые не появляются
-      r_converge_view.Store(r_qr * q_qr + Matrix::Unit(prefix) * shift);
-      q_prefix_view *= q_qr;
+      // оптимизировать константу внутри бессмысленно, потому что рядом
+      // умножение за куб поэтому просто полагаемся на гивенса, что он анулирует
+      // (даже если проходит лишние)
+      r_converge = r_qr * q_qr + Matrix::Unit(rows) * shift;
+      q_prefix *= q_qr;
+    }
+  }
+  return {q_prefix, r_converge};
+}
+
+template <MatrixOrViewType Matrix>
+MatrixElementType<Matrix> WilkinsonShift(const Matrix& matrix) {
+  assert(matrix.Rows() == 2 && matrix.Cols() == 2 &&
+         "Wilkinson shift is for 2x2 matrices");
+  assert(IsSymmetric(matrix) && "Wilkinson shift is for symmetric matrices");
+  using MatrixElement = MatrixElementType<Matrix>;
+  MatrixElement delta = (matrix(0, 0) - matrix(1, 1)) / 2;
+  return matrix(1, 1) - Sign(delta) * matrix(0, 1) /
+                            (std::abs(delta) + std::hypot(delta, matrix(0, 1)));
+}
+
+template <MatrixType Matrix>
+SchurResult<Matrix> SchurWilkinsonQR(
+    const Matrix& matrix, IterCount max_iterations = kDefaultIterations) {
+  assert(IsSquare(matrix) && "Schur Decomposition is for square matrices");
+  assert(IsSymmetric(matrix) &&
+         "Wilkinson shift QR algorithm is for symmetric matrices");
+  auto [q_prefix, r_converge] = HessenbergDecomposition(matrix);
+  auto rows = matrix.Rows();
+  for (Size minor = matrix.Rows(); minor >= 2; --minor) {
+    for (IterCount iter = 0;
+         iter < max_iterations &&
+         !IsEpsilonEqualZero(r_converge(minor - 1, minor - 2));
+         ++iter) {
+      auto shift = WilkinsonShift(
+          matrix.ConstView().SubMatrix(minor - 2, minor - 2, 2, 2));
+      auto [q_qr, r_qr] =
+          GivensQR(r_converge - shift * Matrix::Unit(rows));  // O(n^2)
+      r_converge = r_qr * q_qr + Matrix::Unit(rows) * shift;
+      q_prefix *= q_qr;
     }
   }
   return {q_prefix, r_converge};
--- tests/algo/schur_decomporition/unit_schur_decomposition.cpp
+++ tests/algo/schur_decomporition/unit_schur_decomposition.cpp
@@ -4,12 +4,9 @@
 
 TEST(SchurDecompositionSimple, Unit) {
   linalg_lib::Matrix<double> A = {
-    {2.0, 3.0, 2.0, 2.0, 2.0, 3.0},
-    {3.0, 4.0, 3.0, 2.0, 2.0, 2.0},
-    {2.0, 3.0, 5.0, 3.0, 2.0, 2.0},
-    {2.0, 2.0, 3.0, 6.0, 3.0, 2.0},
-    {2.0, 2.0, 2.0, 3.0, 7.0, 3.0},
-    {3.0, 2.0, 2.0, 2.0, 3.0, 8.0},
+      {2.0, 3.0, 2.0, 2.0, 2.0, 3.0}, {3.0, 4.0, 3.0, 2.0, 2.0, 2.0},
+      {2.0, 3.0, 5.0, 3.0, 2.0, 2.0}, {2.0, 2.0, 3.0, 6.0, 3.0, 2.0},
+      {2.0, 2.0, 2.0, 3.0, 7.0, 3.0}, {3.0, 2.0, 2.0, 2.0, 3.0, 8.0},
   };
   auto [Q, R] = SchurSimpleQR(A);
   EXPECT_TRUE(IsEpsilonEqual(Q * R * Transposed(Q), A));
@@ -21,15 +18,25 @@ TEST(SchurDecompositionSimple, Unit) {
 TEST(SchurDecompositionRayleigh, Unit) {
   // На этой матрице без сдвигов не сходится
   linalg_lib::Matrix<double> A = {
-    {2.0, 3.0, 2.0, 2.0, 2.0, 3.0},
-    {3.0, 2.0, 3.0, 2.0, 2.0, 2.0},
-    {2.0, 3.0, 2.0, 3.0, 2.0, 2.0},
-    {2.0, 2.0, 3.0, 2.0, 3.0, 2.0},
-    {2.0, 2.0, 2.0, 3.0, 2.0, 3.0},
-    {3.0, 2.0, 2.0, 2.0, 3.0, 2.0},
+      {2.0, 3.0, 2.0, 2.0, 2.0, 3.0}, {3.0, 2.0, 3.0, 2.0, 2.0, 2.0},
+      {2.0, 3.0, 2.0, 3.0, 2.0, 2.0}, {2.0, 2.0, 3.0, 2.0, 3.0, 2.0},
+      {2.0, 2.0, 2.0, 3.0, 2.0, 3.0}, {3.0, 2.0, 2.0, 2.0, 3.0, 2.0},
   };
 
   auto [Q, R] = SchurRayleighQR(A);
+  EXPECT_TRUE(IsEpsilonEqual(Q * R * Transposed(Q), A));
+  EXPECT_TRUE(linalg_lib::IsOrthogonal(Q));
+  EXPECT_TRUE(linalg_lib::IsUpperTriangular(R));
+}
+
+TEST(SchurDecompositionWilkinson, Unit) {
+  linalg_lib::Matrix<double> A = {
+      {2.0, 3.0, 2.0, 2.0, 2.0, 3.0}, {3.0, 2.0, 3.0, 2.0, 2.0, 2.0},
+      {2.0, 3.0, 2.0, 3.0, 2.0, 2.0}, {2.0, 2.0, 3.0, 2.0, 3.0, 2.0},
+      {2.0, 2.0, 2.0, 3.0, 2.0, 3.0}, {3.0, 2.0, 2.0, 2.0, 3.0, 2.0},
+  };
+
+  auto [Q, R] = linalg_lib::SchurWilkinsonQR(A);
   EXPECT_TRUE(IsEpsilonEqual(Q * R * Transposed(Q), A));
   EXPECT_TRUE(linalg_lib::IsOrthogonal(Q));
   EXPECT_TRUE(linalg_lib::IsUpperTriangular(R));
